3037664750d2648f55cb509dbf5ae11d5ca06c3a
Parth Mahadik <parthmahadik08@gmail.com>
2025-06-03 00:15:32 +0530

    Weather Web App API used successfully

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css'
+import SearchBox from "./SearchBox"
+import InfoBox from "./InfoBox"
+
+function App() {
+  return (
+    <>
+      <SearchBox/>
+      <InfoBox/>
+    </>
+  )
+}
+
+export default App
