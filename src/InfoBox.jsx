872d9b2c13932cf8cc8c33d877e88e8132ee2e4f
Parth Mahadik <parthmahadik08@gmail.com>
2025-06-03 15:40:30 +0530

    Project completed

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -3,31 +3,28 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import SunnyIcon from '@mui/icons-material/Sunny';
 
-export default function InfoBox() {
+export default function InfoBox({info}) {
     const INIT_URL="https://images.unsplash.com/photo-1701166575979-a0d8813390fb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzA5fHxkdXN0eSUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
-  let info = {
-    city: "Nashik",
-    feelsLike: 24.84,
-    temp: 25.05,
-    tempMin: 25.05,
-    tempMax: 25.05,
-    humidity: 47,
-    weather: "haze",
-  };
 
+    const HOT_URL="https://images.unsplash.com/photo-1524594081293-190a2fe0baae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL="https://images.unsplash.com/photo-1612208695882-02f2322b7fee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL="https://images.unsplash.com/photo-1691265693071-17c35bc733d6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQwfHxyYWluJTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
   return (
     <div className="InfoBox">
-      <h1>WeatherInfo - {info.weather}</h1>
+      <div className='cardContainer'>
       <Card sx={{ maxWidth: 345 }}>
         <CardMedia
           sx={{ height: 140 }}
-          image={INIT_URL}
+          image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL:COLD_URL}
           title="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {info.city}
+            {info.city} {info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <SunnyIcon/>:<AcUnitIcon/>}
           </Typography>
           <Typography variant="body2" sx={{ color: "text.secondary" }} component={"span"}>
             <p>Temperature: {info.temp}&deg;C</p>
@@ -38,6 +35,7 @@ export default function InfoBox() {
           </Typography>
         </CardContent>
       </Card>
+      </div>
     </div>
   );
 }
